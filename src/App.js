5ff9720ca2668f4bb3a9af7fcc7c7a11ea9d518d
ParkJiHo <iks15174@gmail.com>
2021-03-18 00:22:55 +0900

    first commit

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import logo from './logo.svg';
+import './App.css';
+import TOC from "./components/TOC"
+import Content from "./components/Content"
+import Subject from "./components/Subject"
+import React, {Component} from "react"
+
+class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      subject : {title : 'WEB', sub : 'world wide web!'},
+      contents :[
+        {id:1, title:"HTML", desc:"HTML is for information"},
+        {id:2, title:"CSS", desc:"CSS is for design"},
+        {id:3, title:"JavaScript", desc:"JavaScript is for interactive"},
+      ]
+
+    }
+  }
+  render(){
+    return (
+      <div className="App">
+        <Subject title={this.state.subject.title} sub={this.state.subject.sub}></Subject>
+        <TOC data={this.state.contents}></TOC>
+        <Content title="HTML" desc="HTML is HyperText Markup Language."></Content>
+      </div>
+    );
+  }
+}
+
+export default App;
